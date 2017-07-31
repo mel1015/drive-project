5f213d401356729306a487c1ea0be64ac9201bf1
신광식 <sks1015sks@naver.com>
2017-07-31 18:54:43 +0900

    main.php, sign_up.php - Bootstrap으로 디자인 변경

--- main.php
+++ main.php
@@ -1,33 +1,46 @@
 <?php
+  require("config/config.php");
+  require("lib/db.php");
+
   session_start();   //세션의 시작
 
-  $conn = mysqli_connect('localhost', 'root', 4987);
-  mysqli_select_db($conn, "cloud");
-  $result = mysqli_query($conn, "SELECT * FROM data");
+  //데이터베이스 연결
+  $conn = db_init($config["host"], $config["duser"],
+    $config["dpw"], $config["dname"]);
+  $result = mysqli_query($conn, "SELECT * FROM user");
 
+  //POST를 이용해 전달해준 값이 있는지 확인 후
+  //전달받은 아이디랑 비밀번호로 로그인
   if($_SERVER["REQUEST_METHOD"] == "POST")
   {
     $myusername = $_POST['username'];
     $mypassword = $_POST['password'];
     $check = true;
 
-    while($row = mysqli_fetch_assoc($result)) {
-
-      if($row['username'] == $myusername &&
-      $row['password'] == $mypassword) {
-
+    while($row = mysqli_fetch_assoc($result))
+    {
+      if($row['name'] == $myusername &&
+      $row['password'] == $mypassword)
+      {
         $_SESSION['myusername']=$myusername;
         header('Location: http://localhost/cloud/welcome.php');
       }
-      else {
+      else
         $check = false;
-      }
     }
     if($check == false)
-    {
       echo '<script>alert("틀린 입력입니다.")</script>';
+  }
+
+  //회원가입 세션에서 넘겨준 값으로 회원가입 성공 알림창
+  if(isset($_SESSION['id_check']))
+  {
+    $id_check = $_SESSION['id_check'];
+    if($id_check == false)
+    {
+      echo '<script>alert("회원가입 성공!!!")</script>';
+      session_unset();
     }
-    // header('Location: http://localhost/cloud/main.php');
   }
 ?>
 
@@ -36,19 +49,48 @@
 
 <head>
   <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
   <title>Login Page</title>
+
   <link rel="stylesheet" type="text/css" href="http://localhost/cloud/style.css">
+  <link href="http://localhost/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
-<body class = "main">
-  <p><b>아이디와 패스워드를 입력해주세요</b></p>
-  <form action="" method="post">
-    <p><label>아 이 디 : </label><input type="text" name="username" required></p>
-    <p><label>패스워드 : </label><input type="password" name="password" required></p>
-    <input type="submit" value="로그인">
-    <input type="button" value="회원가입"
-      onclick="location.href='http://localhost/cloud/sign_up.php'">
-  </form>
+<body>
+  <div class="container">
+
+    <header class="jumbotron text-center">
+      <img src="https://avatars2.githubusercontent.com/u/26675061?v=4&s=400"
+      alt="프로필" class="img-circle" id="logo">
+      <h2><a href="http://localhost/cloud/main.php">My-Drive</a></h2>
+    </header>
+
+    <article>
+
+      <h3>로그인</h3>
+      <form action="" method="post">
+
+        <div class="form-group">
+          <label for="form-name">아이디</label>
+          <input type="text" class="form-control" name="username"
+            id="from-name" placeholder="아이디" required>
+        </div>
+        <div class="form-group">
+          <label for="form-password">비밀번호</label>
+          <input type="password" class="form-control" name="password"
+            id="from-password" placeholder="비밀번호" required>
+        </div>
+
+        <input type="submit" class="btn btn-success btn-lg" value="로그인">
+        <input type="button" class="btn btn-default btn-lg" value="회원가입"
+        onclick="location.href='http://localhost/cloud/sign_up.php'">
+      </form>
+
+    </article>
+
+  </div>
 
 </body>
 
--- sign_up.php
+++ sign_up.php
@@ -4,7 +4,8 @@
   if(isset($_SESSION['id_check']))
   {
     $id_check = $_SESSION['id_check'];
-    if($id_check == true) {
+    if($id_check == true)
+    {
       echo '<script>alert("중복된 아이디입니다.")</script>';
       session_unset();
     }
@@ -16,18 +17,48 @@
 
 <head>
   <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
   <title>Sign Up Page</title>
+
   <link rel="stylesheet" type="text/css" href="http://localhost/cloud/style.css">
+  <link href="http://localhost/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
-<body class = "main">
-  <p><b>새로운 계정</b></p>
-  <form action="process.php" method="post">
-    <p><label>아 이 디 : </label><input type="text" name="sign_up_username" required></p>
-    <p><label>패스워드 : </label><input type="password" name="sign_up_password" required></p>
-    <input type="submit" value="회원가입">
-  </form>
-  <a href="http://localhost/cloud/main.php">홈으로</a>
+<body>
+  <div class="container">
+
+    <header class="jumbotron text-center">
+      <img src="https://avatars2.githubusercontent.com/u/26675061?v=4&s=400"
+      alt="프로필" class="img-circle" id="logo">
+      <h2><a href="http://localhost/cloud/main.php">My-Drive</a></h2>
+    </header>
+
+    <article>
+
+      <h3>새로운 계정</h3>
+      <form action="process.php" method="post">
+        
+        <div class="form-group">
+          <label for="form-name">아이디</label>
+          <input type="text" class="form-control" name="sign_up_username"
+            id="from-name" placeholder="아이디" required>
+        </div>
+        <div class="form-group">
+          <label for="form-password">비밀번호</label>
+          <input type="text" class="form-control" name="sign_up_password"
+            id="from-password" placeholder="비밀번호" required>
+        </div>
+
+        <input type="submit" class="btn btn-success btn-lg" value="회원가입">
+        <input type="button" class="btn btn-default btn-lg" value="홈으로"
+        onclick="location.href='http://localhost/cloud/main.php'">
+      </form>
+
+    </article>
+
+  </div>
 </body>
 
 </html>
